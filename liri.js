3d65309fe49dc3d305190e845d4be61366ba8e8c
Anne Simon <Anne_Simon@camh.ca>
2018-07-25 12:53:32 -0400

    my-tweets command working!

--- liri.js
+++ liri.js
@@ -1,29 +1,70 @@
 require("dotenv").config();
 
+
 //import keys.js file stored in variable
 var keys = require("./keys.js");
 
+
+//node packages
+var fs = require("fs");
+var request = require("request");
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 
- //pass the config (keys) of Twitter and Spotify applications in keys.js to 'Twitter' and 'Spotify', respectively
+//pass the config (keys) of Twitter and Spotify apps in keys.js to 'Twitter' and 'Spotify', respectively
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 
-/*node liri.js my-tweets
-This will show your last 20 tweets and when they were created at in your terminal/bash window.*/
+//store all arguments in an array
+var nodeArgs = process.argv;
 
-var params = {
-	count:20,
-	exclude_replies:true
+var command = "";
+
+// commands: my-tweets, spotify-this-song, movie-this, do-what-it-says
+
+for (var i = 2; i < nodeArgs.length; i++) {
+	command = command + " " + nodeArgs[i];
 }
 
-client.get('statuses/home_timeline', params, function(error, tweets, response){
-	if(error) throw error;
-	
-	console.log(tweets);
+console.log("Command: " + command);
 
-	//console.log(response);
-});
 
+/**** node liri.js my-tweets ****/
+
+if (command.includes("my-tweets")) {
+
+	var params = {
+		count:20,
+		exclude_replies:true
+	}
+
+	client.get('statuses/home_timeline', params, function(error, tweets){
+		if(error) throw error;
+		
+		console.log(tweets);
+
+	});
+}
+
+
+
+
+//this switch-case will direct which function gets run
+// switch(command) {
+// 	case "my-tweets":
+// 	tweets();
+// 	break;
+
+// 	case "spotify":
+// 	spotify();
+// 	break;
+
+// 	case "movie":
+// 	movie();
+// 	break;
+
+// 	case "random":
+// 	random();
+// 	break;
+// }
