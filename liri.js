f41ab28c0047a853f2b92cb99de3ee965ec58499
Anne Simon <Anne_Simon@camh.ca>
2018-07-25 11:52:16 -0400

    added code to show latest tweets on log

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var Twitter = require("twitter");
-
+var Spotify = require("node-spotify-api");
 
  //pass the config (keys) of Twitter and Spotify applications in keys.js to 'Twitter' and 'Spotify', respectively
 var spotify = new Spotify(keys.spotify);
@@ -15,15 +15,15 @@ var client = new Twitter(keys.twitter);
 This will show your last 20 tweets and when they were created at in your terminal/bash window.*/
 
 var params = {
-	q: '',
-	count:20;
+	count:20,
+	exclude_replies:true
 }
 
-client.get('search/tweets', params, function(error, tweets, response){
+client.get('statuses/home_timeline', params, function(error, tweets, response){
 	if(error) throw error;
 	
 	console.log(tweets);
-	
+
 	//console.log(response);
 });
 
